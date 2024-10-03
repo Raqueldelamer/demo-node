0648be56ce9b91883669560d2458cde658eba8be
Raquel Marie <raqueldelamer@gmail.com>
2024-10-02 21:34:07 -0500

    update index & opinionPoll

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ opinionPoll();
 
 const response = await input({message: 'What is your favorite song?'});
 console.log(`Great choice! ${response} is a classic!`);
+
+const hobby = await input({message: 'What is your favorite hobby?'});
+console.log(`${hobby} is a great way to spend time!`);
+
+const programming = await input({message: 'What is your favorite programming language?'});
+console.log(`${programming} is difficult. Keep at it!`);
--- opinionPoll.js
+++ opinionPoll.js
@@ -1,6 +1,12 @@
-
 function opinionPoll() {
     console.log("Bonjour from opinionPoll.js")
 }
 
-export { opinionPoll };
+function hobby() {
+    console.log("Having a hobby is important for having peace of mind.")
+}
+
+function programming() {
+    console.log("Keep at it!")
+}
+export { opinionPoll, hobby, programming };
